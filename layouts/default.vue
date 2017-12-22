667b1ee7435906510c996b35d27285f9d1194719
bad4iz <bad4iz@gmail.com>
2017-12-22 21:13:18 +0400

    add vuetify

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
+    <ToolBar/>
     <nuxt/>
   </div>
 </template>
 
+<script>
+import ToolBar from '../components/Toolbar.vue';
+export default {
+  components: {
+    ToolBar
+  }
+}
+</script>
+
 <style>
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -16,7 +27,9 @@ html {
   box-sizing: border-box;
 }
 
-*, *:before, *:after {
+*,
+*:before,
+*:after {
   box-sizing: border-box;
   margin: 0;
 }
